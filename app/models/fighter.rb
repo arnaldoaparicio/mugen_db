88e2c23c653cf1d10fa0bc9b6018749f158ecf80
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-06-15 16:37:37 -0400

    Fix spacing and indentation

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,5 +1,4 @@
 class Fighter < ApplicationRecord
-    validates_presence_of :name
-    belongs_to :character
-    has_many :fighter_variants
+  belongs_to :character
+  has_many :fighter_variants
 end
