ac8fde9e69e1bc83e7b6680bf1a0d47bc2ab04eb
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-06-15 17:16:42 -0400

    Add relationship tests

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Character, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many :fighters }
+  end
 end
