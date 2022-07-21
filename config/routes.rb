3b8f47f34580daf5bc18f40ea833692762a63249
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-21 15:35:54 -0400

    Modify route to create fighter_variant

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :characters, only: [:index, :show]
+      # resources :fighter_variants, only: [:create]
     end
   end
   get "/api/v1/characters/:series_id/:fighter_id", to: "api/v1/characters#index"
+  post "/api/v1/fighter_variants", to: "api/v1/fighter_variants#create"
+
 end
