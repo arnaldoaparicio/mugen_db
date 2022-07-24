4a93b06ececafaf228b49064f8772f09fbb044bb
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-23 21:20:52 -0400

    Fix spacing

--- app/controllers/api/v1/fighter_variants_controller.rb
+++ app/controllers/api/v1/fighter_variants_controller.rb
@@ -1,20 +1,19 @@
-class Api::V1::FighterVariantsController < ApplicationController 
+class Api::V1::FighterVariantsController < ApplicationController
+  def new
+  end
 
-    def new
+  def create
+    fighter_variant = FighterVariant.new(fighter_variant_params)
+    if fighter_variant.save
+      render json: FighterVariantSerializer.new(fighter_variant), status:201
+    else
+      render status: 400
     end
+  end
 
-    def create
-        fighter_variant = FighterVariant.new(fighter_variant_params)
-        if fighter_variant.save
-            render json: FighterVariantSerializer.new(fighter_variant), status:201
-        else
-            render status: 400
-        end
-    end
+  private
 
-    private 
-
-    def fighter_variant_params
-        params.require(:fighter_variant).permit(:name, :author, :website, :game_name, :fighter_id, :avatar)
-    end    
+  def fighter_variant_params
+    params.require(:fighter_variant).permit(:name, :author, :website, :game_name, :fighter_id, :avatar)
+  end
 end
