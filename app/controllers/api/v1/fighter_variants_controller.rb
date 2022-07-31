c17d3eef5d37ab8486161c138ad15358f4045c49
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-30 23:02:16 -0400

    Remove pry

--- app/controllers/api/v1/fighter_variants_controller.rb
+++ app/controllers/api/v1/fighter_variants_controller.rb
@@ -5,6 +5,8 @@ class Api::V1::FighterVariantsController < ApplicationController
   def create
     fighter_variant = FighterVariant.new(fighter_variant_params)
     if fighter_variant.save
+      # fighter_variant.avatar.attach(params["avatar"])
+      # fighter_variant.avatar.attach(io: File.open(params["avatar"], filename: 'mugen20.png'))
       render json: FighterVariantSerializer.new(fighter_variant), status:201
     else
       render status: 400
@@ -14,6 +16,6 @@ class Api::V1::FighterVariantsController < ApplicationController
   private
 
   def fighter_variant_params
-    params.require(:fighter_variant).permit(:name, :author, :website, :game_name, :fighter_id, :avatar)
+    params.require(:fighter_variant).permit(:name, :author, :website, :game_name, :image, :fighter_id, :avatar)
   end
 end
