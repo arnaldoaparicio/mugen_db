643815d82193547e7ddeb787e9a2aace6b94ba04
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-30 23:03:30 -0400

    Replace avatar with image_url

--- app/serializers/fighter_variant_serializer.rb
+++ app/serializers/fighter_variant_serializer.rb
@@ -1,4 +1,4 @@
 class FighterVariantSerializer
   include JSONAPI::Serializer
-  attributes :name, :author, :website, :game_name, :image
+  attributes :name, :author, :website, :game_name, :image, :image_url
 end
