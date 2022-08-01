f51c3683a1e35432630445af622a8efd8fbe7540
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-31 20:34:20 -0400

    Fix spacing and indentation

--- app/models/fighter_variant.rb
+++ app/models/fighter_variant.rb
@@ -1,12 +1,9 @@
 class FighterVariant < ApplicationRecord
-    belongs_to :fighter
+  belongs_to :fighter
 
-    has_one_attached :avatar
+  has_one_attached :avatar
 
-    def image_url
-      if avatar.attached?
-        avatar.blob.service_url
-    end
+  def image_url
+    avatar.blob.service_url if avatar.attached?
   end
-
 end
