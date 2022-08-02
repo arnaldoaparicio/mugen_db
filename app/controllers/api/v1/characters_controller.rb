49559835716182ae1e8c5adbef4200d877c0c878
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-02 17:31:51 -0400

    Add create method and strong params

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CharactersController < ApplicationController
   def index
-    
+
     if params[:series_id].present? && params[:fighter_id].present?
       series = Character.find(params[:series_id])
       fighter = series.fighters.find(params[:fighter_id])
@@ -14,5 +14,20 @@ class Api::V1::CharactersController < ApplicationController
   def show
     character = Character.find(params[:id])
     render json: FighterSerializer.new(character.fighters)
+  end
+
+  def create
+    character = Character.new(character_params)
+    if character.save
+      render json: CharacterSerializer.new(character), status: 201
+    else
+      render status: 400
+    end
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:origin)
   end
 end
