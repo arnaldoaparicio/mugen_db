a3fbecc8b8a28f0582cc515b7011df603d8992cd
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-16 07:45:28 -0400

    Rewrite post routes for fighters and fighter_variants

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :characters, only: [:index, :show, :create]
+      resources :fighters, only: [:create]
+      resources :fighter_variants, only: [:create]
     end
   end
-  get "/api/v1/characters/:series_id/:fighter_id", to: "api/v1/characters#index"
-  post "/api/v1/fighter_variants", to: "api/v1/fighter_variants#create"
-  post '/api/v1/fighters', to: 'api/v1/fighters#create'
+  get '/api/v1/characters/:series_id/:fighter_id', to: 'api/v1/characters#index'
+  # post '/api/v1/fighter_variants', to: 'api/v1/fighter_variants#create'
+  # post '/api/v1/fighters', to: 'api/v1/fighters#create'
 end
