52645f9162c871badc466fd541761abeb5863368
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-16 07:51:22 -0400

    Fix spacing'

--- spec/models/fighter_variant_spec.rb
+++ spec/models/fighter_variant_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe FighterVariant, type: :model do
   describe 'relationships' do
-    it { should belong_to :fighter}
+    it { should belong_to :fighter }
   end
 end
