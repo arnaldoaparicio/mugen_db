a7359e9b629cd323f96c2a2c7b6419c106ae94d0
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-12-11 11:51:48 -0500

    Fix spacing

--- app/controllers/api/v1/fighter_variants_controller.rb
+++ app/controllers/api/v1/fighter_variants_controller.rb
@@ -7,14 +7,13 @@ class Api::V1::FighterVariantsController < ApplicationController
     if fighter_variant.save
       # fighter_variant.avatar.attach(params["avatar"])
       # fighter_variant.avatar.attach(io: File.open(params["avatar"], filename: 'mugen20.png'))
-      render json: FighterVariantSerializer.new(fighter_variant), status:201
+      render json: FighterVariantSerializer.new(fighter_variant), status: 201
     else
       render status: 400
     end
   end
 
   private
-
   def fighter_variant_params
     params.require(:fighter_variant).permit(:name, :author, :website, :game_name, :image, :fighter_id, :avatar)
   end
