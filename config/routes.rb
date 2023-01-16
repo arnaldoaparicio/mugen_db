ae6bb84d6fd068eca920c3d33bdb53b14047f57d
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-16 14:09:23 -0500

    Add login route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     end
   end
   get '/api/v1/characters/:series_id/:fighter_id', to: 'api/v1/characters#index'
+  post '/api/v1/login', to: 'api/v1/users#login'
   # post '/api/v1/fighter_variants', to: 'api/v1/fighter_variants#create'
   # post '/api/v1/fighters', to: 'api/v1/fighters#create'
 end
