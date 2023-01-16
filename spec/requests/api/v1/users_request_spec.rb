a57e646001d926be36aa0f6d2f264a2b597769ce
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-16 14:15:48 -0500

    Fix spacing

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -13,14 +13,9 @@ RSpec.describe 'User API' do
   end
 
   it 'logins to existing account' do
-    user = User.create(email: 'jake@mail.com',
-                       password: 'blahblah',
-                       password_confirmation: 'blahblah',
-                       admin: true)
-    user_params = {
-      email: 'jake@mail.com',
-      password: 'blahblah'
-    }
+    user = User.create(email: 'jake@mail.com', password: 'blahblah', password_confirmation: 'blahblah', admin: true)
+
+    user_params = { email: 'jake@mail.com', password: 'blah' }
 
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
@@ -30,14 +25,8 @@ RSpec.describe 'User API' do
   end
 
   it 'fails to login with wrong password' do
-    user = User.create(email: 'jake@mail.com',
-                       password: 'blahblah',
-                       password_confirmation: 'blahblah',
-                       admin: true)
-    user_params = {
-      email: 'jake@mail.com',
-      password: 'blah'
-    }
+    user = User.create(email: 'jake@mail.com', password: 'blahblah', password_confirmation: 'blahblah', admin: true)
+    user_params = { email: 'jake@mail.com', password: 'blah' }
 
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
@@ -47,19 +36,13 @@ RSpec.describe 'User API' do
   end
 
   it 'fails to login with email' do
-    user = User.create(email: 'jake@mail.com',
-                       password: 'blahblah',
-                       password_confirmation: 'blahblah',
-                       admin: true)
-    user_params = {
-      email: 'ryan@mail.com',
-      password: 'blahblah'
-    }
+    user = User.create(email: 'jake@mail.com', password: 'blahblah', password_confirmation: 'blahblah', admin: true)
+    user_params = { email: 'ryan@mail.com', password: 'blahblah' }
 
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
     post '/api/v1/login', headers: headers, params: JSON.generate(user: user_params)
-    
+
     expect(response.status).to eq(400)
   end
 end
