d49366ea874c2859c519ca32d6655008ac18b073
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-15 23:32:37 -0500

    Add test to create user

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'User API' do
-    xit 'creates a new user' do
-        user_params = { email: 'jake@mail.com', }
+    it 'creates a new user' do
+        user_params = { email: 'jake@mail.com',
+                        password: 'blahblah',
+                        password_confirmation: 'blahblah',
+                        admin: true 
+                      }
+        headers = { 'CONTENT_TYPE' => 'application/json'}
+
+        post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
+
+        expect(response.status).to eq(201)
     end
 end
