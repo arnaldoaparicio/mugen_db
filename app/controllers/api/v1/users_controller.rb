a5e50e768bde221c2afe55ffe49369ed71539d5a
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-16 12:10:57 -0500

    Complete create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-    def create 
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: UserSerializer.new(user), status: 201
+        else
+            render status: 400
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :admin)
     end
 
 end
