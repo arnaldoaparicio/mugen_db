4e19b91db2cf44147837ae30fee85110e0a22477
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-17 18:26:29 -0500

    Add show method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,11 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: UserSerializer.new(user)
+  end
+
   def login
     user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
