076eef6bfc53d3394021ea8b6b3c57c6d2cd91f4
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-28 14:29:06 -0400

    Change scope name to 'origin_name'

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,7 @@
 class Character < ApplicationRecord
   has_many :fighters
   validates_presence_of :origin
-  
-  scope :character_name , -> { order(origin: :asc) }
+  scope :origin_name , -> { order(origin: :asc) }
 
   def series_fighters
     fighters.each do |fighter|
