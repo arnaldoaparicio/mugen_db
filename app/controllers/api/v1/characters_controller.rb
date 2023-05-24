88550735f7f706a9b466d02cd128809230f0a38e
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-24 18:48:41 -0400

    Add 'update' method

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -24,6 +24,16 @@ class Api::V1::CharactersController < ApplicationController
     end
   end
 
+  def update
+    # require 'pry'; binding.pry
+    character = Character.find(params[:id])
+    if character.update(character_params)
+      render json: CharacterSerializer.new(character)
+    else
+      render status: 422
+    end
+  end
+
   private
 
   def character_params
