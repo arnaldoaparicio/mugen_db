3f54336ff9584facdbee1203cbae96b5fb76f0af
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-27 11:52:48 -0400

    Add validations for fighter variant name, author, website, and game_name

--- app/models/fighter_variant.rb
+++ app/models/fighter_variant.rb
@@ -1,6 +1,11 @@
 class FighterVariant < ApplicationRecord
   belongs_to :fighter
 
+  validates_presence_of :name
+  validates_presence_of :author
+  validates_presence_of :website
+  validates_presence_of :game_name
+
   has_one_attached :avatar
 
   def image_url
