fe44990ba00db492a392b263413f31c55e515656
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-27 11:46:31 -0400

    Add test to check validation of character origin

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Character, type: :model do
   describe 'relationships' do
     it { should have_many :fighters }
+    it { should validate_presence_of(:origin)  }
   end
 end
