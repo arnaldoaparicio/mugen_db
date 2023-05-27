8014757c14d755932e5f8054fa8ba9fa00e7efa4
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-27 11:48:11 -0400

    Add test to check for validation of fighter name

--- spec/models/fighter_spec.rb
+++ spec/models/fighter_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Fighter, type: :model do
   describe 'relationships' do
     it { should belong_to :character }
     it { should have_many :fighter_variants }
+    it { should validate_presence_of :name}
   end
 end
