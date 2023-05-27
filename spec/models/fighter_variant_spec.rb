8ec93ec9ed9480ecae6e6ceafa4eacca4f8eef5b
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-27 11:51:46 -0400

    Add test to check validations for fighter variant name, author, website, and game name

--- spec/models/fighter_variant_spec.rb
+++ spec/models/fighter_variant_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe FighterVariant, type: :model do
   describe 'relationships' do
     it { should belong_to :fighter }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :author }
+    it { should validate_presence_of :website }
+    it { should validate_presence_of :game_name }
+  end
 end
