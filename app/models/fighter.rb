b803622056713f8d85d062c4e50f011c841822f9
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-27 11:47:56 -0400

    Add validation for fighter name

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,5 +1,6 @@
 class Fighter < ApplicationRecord
   belongs_to :character
+  validates_presence_of :name
   has_many :fighter_variants , -> {includes(:fighter)}
 
   scope :alphabetical_name , -> { order(name: :asc) }
