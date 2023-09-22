c32ccf6824bb1efd0eee27acd4d96d55abf91022
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-09-22 15:27:44 -0400

    Change deprecated method within 'image_url' method

--- app/models/fighter_variant.rb
+++ app/models/fighter_variant.rb
@@ -9,6 +9,6 @@ class FighterVariant < ApplicationRecord
   has_one_attached :avatar
 
   def image_url
-    avatar.blob.service_url if avatar.attached?
+    avatar.blob.url if avatar.attached?
   end
 end
