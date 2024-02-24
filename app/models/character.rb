032f5f32688830fae4d798603140f5d4786b2724
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-02-24 15:40:55 -0500

    Add validation for 'origin_tag' attribute

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ApplicationRecord
   has_many :fighters, -> {includes(:character)}
   validates_presence_of :origin
+  validates_presence_of :origin_tag
   scope :origin_name , -> { order(origin: :asc) }
 end
