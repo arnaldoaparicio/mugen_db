a29a21f6103408c9eb66a1fc586518e105637a3e
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-02-24 15:37:19 -0500

    Add 'origin_tag' attribute

--- db/migrate/20220528164645_create_characters.rb
+++ db/migrate/20220528164645_create_characters.rb
@@ -2,6 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
       t.string :origin
+      t.string :origin_tag
 
       t.timestamps
     end
