157747ab78a54f13d31a0dd296d6c3dd9571f9dd
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-02-26 16:35:23 -0500

    Add 'after_validation' callback and add 'set_slug' method

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,12 @@ class Character < ApplicationRecord
   validates_presence_of :origin
   validates_presence_of :origin_tag
   scope :origin_name , -> { order(origin: :asc) }
+
+  after_validation :set_slug, only: [:create, :update]
+
+  private
+
+  def set_slug
+    self.slug = title.to_s.parameterize
+  end
 end
