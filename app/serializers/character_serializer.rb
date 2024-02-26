8be4361995505307ee42683be4fe50cf8259fffe
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-02-26 17:12:19 -0500

    Add 'slug_url' to serializer

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -2,4 +2,8 @@ class CharacterSerializer
   include JSONAPI::Serializer
   set_type :characters
   attributes :origin, :origin_tag
+
+  attribute :slug_url do |object|
+    object.to_param
+  end
 end
